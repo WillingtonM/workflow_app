475b3ef527e5e04396e79e0c5bd7bdda322e0fa5
Willington <tralon.manage@gmail.com>
2023-12-08 08:25:34 +0200

    Team page update

--- src/apps/app-public/model/about.php
+++ src/apps/app-public/model/about.php
@@ -1,13 +1,1 @@
 <?php
-
-$practice = '';
-
-if(isset($_GET['story'])){
-  $practice = 'story';
-}elseif (isset($_GET['promise'])) {
-  $practice = 'promise';
-}elseif (isset($_GET['testimonials'])) {
-  $practice = 'testimonials';
-}elseif (isset($_GET['estate_planning'])) {
-  $practice = 'estate_planning';
-}
